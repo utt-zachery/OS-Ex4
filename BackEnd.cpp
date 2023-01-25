5f4a82c25e7f0648d7dc24f009cfddb4c1ee15af
Zachery Utt <55007266+utt-zachery@users.noreply.github.com>
2023-01-24 22:28:56 -0500

    Add interfaces for Ex4 - Unit Testing

---
+++ BackEnd.cpp
@@ -0,0 +1,25 @@
+//Author: Zachery Utt
+
+#include "BackEnd.h"
+
+//Dummy implementation of flightBookingQuery so you can check that your tests can compile
+
+//NOTE: you should not be implementing these methods! You should be writing tests for them.
+
+
+
+//flightBookingQuery
+//Always returns an empty list
+//The final implementation will return a vector<Booking> per the specifications
+
+std::vector<Booking> flightBookingQuery(std::vector<Flight*>& allFlights, Airport& source, Airport& dest) {
+	std::vector<Booking> toReturn;
+	return toReturn;
+}
+
+//purchaseBooking
+//Does nothing
+//The final implementation will process a Booking when it is purchased
+void purchaseBooking(Booking& toPurchase) {
+
+}
---
+++ Flight.h
@@ -0,0 +1,35 @@
+//Author: Zachery Utt
+
+#pragma once
+
+#include "Airport.h"
+
+//A Flight is a scheduled flight from a source airport to a destination airport
+
+class Flight {
+
+private:
+	int capacity;
+	int takeoffTime;
+	Airport& source;
+	Airport& destination;
+
+public:
+	Flight(int capacity, int takeoffTime, Airport& source, Airport& destination) :
+			capacity(capacity), takeoffTime(takeoffTime), source(source), destination(destination) { };
+	
+	int getTakeoffHour() {
+		return this->takeoffTime;
+	}
+
+
+	int getCapacity() {
+		return this->capacity;
+	}
+
+	void decrementCapacity() {
+		this->capacity--;
+	}
+
+
+};

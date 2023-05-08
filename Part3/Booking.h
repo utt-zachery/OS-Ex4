01cf85aa6ef3c55f62acaedaa4b4f688c0142557
Zachery Utt <55007266+utt-zachery@users.noreply.github.com>
2023-05-07 22:17:36 -0400

    Update Booking.h

--- Part3/Booking.h
+++ Part3/Booking.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include <vector>
+#include <set>
 #include "Flight.h"
 
 // A Booking object represents one or many flights (in the case of a connection)
@@ -31,7 +32,7 @@ public:
 	//two bookings are equivalent iff they contain equivalent vectors with the same flights in the same order
 	bool operator==(const Booking& rhs) const
 	{
-	     return this->flights == rhs.flights;
+	     return set<Flight*>(this->flights.start(), this->flights.end()) == set<Flight*>(rhs.start(), rhs.end());
 	}
 
 };
